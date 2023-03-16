0dca45a8ababdb2956a8303805c5f0f42fbf3f8c
Eric Collard <ruffyreborn@gmail.com>
2023-03-15 21:13:33 -0500

    Completed Commonewealth Coders Assignment

---
+++ script.js
@@ -0,0 +1,50 @@
+// QUESTION 1
+// const
+
+// QUESTION 2
+let num = 20;
+
+// QUESTION 3
+num = 15;
+
+// QUESTION 4
+num = num + 7;
+
+// QUESTION 5
+num --
+
+// QUESTION 6
+num = "This is not a number"
+
+// QUESTION 7
+let blank;
+
+// QUESTION 8
+console.log(blank);
+
+// QUESTION 9
+blank = null
+
+// BONUS 1
+const quarter = .25, dime = .10, nickel = .05, penny = .01;
+
+// BONUS 2
+console.log(quarter, dime, nickel, penny);
+
+// BONUS 3
+let letters = "qwertyuiop"
+
+// BONUS 4
+function getChar(numeral) {
+    let char = String.fromCharCode(numeral + 79);
+    char = char.toLowerCase();
+    return char;
+}
+console.log(getChar(letters[9].length));
+
+
+
+
+
+
+
